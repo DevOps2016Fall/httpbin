b06ab96994bcff52e60734c2068a288b1d651b64
WeiFoo <wfu@ncsu.edu>
2016-10-17 23:01:07 -0400

    gate

--- gate.py
+++ gate.py
@@ -1,15 +1,22 @@
 from __future__ import print_function, division
 import pdb
+import subprocess
 
 conv = 200
-coverage = 0.7
+coverage = 0.9
 with open("pylint.out", "r")as f:
   for line in f.readlines():
     if line[1:11] == "convention":
       if int(line[13:16]) > conv:
-        print("Static analysis convetion violation is higher than ", conv, ".")
-        print("Commit Failed!")
+        x = "Static analysis convetion violation is higher than "+str(conv)
+        subprocess.call(["echo", x])
+        subprocess.check_call(["echo","Commit Failed!!!"])
+        # subprocess.call("exit 1", shell=True)
         exit(1)
+
+        # # print("Static analysis convetion violation is higher than ", conv, ".")
+        # print("Commit Failed!")
+        # exit(1)
 print("Static analysis convetion violation checking passed!")
 
 with open("coverage.xml", "r") as f:
@@ -17,8 +24,9 @@ with open("coverage.xml", "r") as f:
     if line[26:35] == "line-rate":
       rate = line[37:43]
       if float(rate) < coverage:
-        print("Average code coverage is less than ",coverage,".")
-        print("Commit Failed!")
+        x = "Average code coverage is less than "+str(coverage)+"."
+        subprocess.call(["echo", x])
+        subprocess.check_call(["echo","Commit Failed!!!"])
         exit(1)
 print("Average code coverage checking passed!")
 
